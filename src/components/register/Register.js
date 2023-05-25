ba2e4b045c11e7b89d78e6d2aebb2fd6f9220e45
sugesh chandra m k <sugeshc389@gmail.com>
2023-05-25 09:01:12 +0530

    Rgister form design compleated

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './Register.css';
 
 function Register() {
     const [name, setName] = useState('');
@@ -12,39 +13,45 @@ function Register() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="name">Name:</label>
-                <input
-                    type="text"
-                    id="name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    required
-                />
-            </div>
-            <div>
-                <label htmlFor="email">Email:</label>
-                <input
-                    type="email"
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-            </div>
-            <div>
-                <label htmlFor="password">Password:</label>
-                <input
-                    type="password"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-            </div>
-            <button type="submit">Register</button>
-        </form>
+        <div className='maindiv'>
+
+            <form onSubmit={handleSubmit} className='form'>
+                <div className='sm'>
+                    <label htmlFor="name">Name:</label> <br/>
+                    <input
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                        placeholder='name'
+                    />
+                </div>
+                <div className='sm'>
+                    <label htmlFor="email" >Email:</label><br/>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        
+                    />
+                </div>
+                <div className='sm'>
+                    <label htmlFor="password">Password:</label><br/>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        placeholder='password'
+                    />
+                </div><br/><br/><br/>
+                <button type="submit">Register</button>
+            </form>
+        </div>
     );
 }
 
