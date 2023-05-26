cf6dfbee0fe641ffe041c35aebb3115ec19a94b2
sugesh chandra m k <sugeshc389@gmail.com>
2023-05-26 10:43:15 +0530

    Just edited

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,26 +1,46 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Home.css'
 import { Link } from 'react-router-dom'
+import { myContext } from '../Context';
 
 function Home() {
+    const user = useContext(myContext)
+    console.log(user);
     return (
         <div className='mdiv'>
-
-            <header>
-
-
-                <h1 style={{ color: 'gold' }}>Home</h1>
-
-                <Link to={'/register'}>Register</Link>
-                <Link to={'/login'}>Login</Link>
+            {
+                user.name !== '' ?
 
 
+                    <div>
+                        <header>
 
-            </header>
+                            <h1 style={{ color: 'gold' }}>Home</h1>
+                            <Link to={'/register'}>Register</Link>
+                            <Link to={'/login'}>Login</Link>
+
+                        </header>
+                        <h1>Welcome,{user.name}</h1></div>
+                    :
+                    <header>
+
+                        <h1 style={{ color: 'gold' }}>Home</h1>
+                        <Link to={'/register'}>Register</Link>
+                        <Link to={'/login'}>Login</Link>
+
+                    </header>}
 
 
         </div>
+
+
     )
 }
 
-export default Home 
+export default Home
+
+
+
+
+
+
